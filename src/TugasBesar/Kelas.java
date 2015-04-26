3b01d517f364393521cba51b14d3a2baef8159ee
AkhmadZulfikar <lmj.zulfikar@gmail.com>
2015-04-26 17:29:48 +0700

    Update Kelas.java

--- src/TugasBesar/Kelas.java
+++ src/TugasBesar/Kelas.java
@@ -52,7 +52,7 @@ public void setLokasiruang(String lokasiruang) {
 }
 
 
-//Setter n Getter untuk Kondisi Ruang Kelas
+//Setter n Getter untuk Kondisi Ruang Kelas, ini yang dibuat zulfikar
 public Double getPanjangruang() {
 	return panjangruang;
 }
