2791f681511602f59ca09795a450799534b8b100
AkhmadZulfikar <lmj.zulfikar@gmail.com>
2015-05-17 20:25:08 +0700

    Ini class buat pewarisannya KONDISI SARANA
    nanti ini akan di implement ke berbagai analisis yang ada di KONDISI SARANA , seperti stop kontak, lampu, LCD, dll.

---
+++ src/TugasBesar/Sarana.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasBesar;
+
+/**
+ *
+ * @author OWNER
+ */
+public abstract class Sarana extends DataKelas {
+    
+String x = "Sesuai", y = "Tidak Sesuai"; 
+String a1,a2,a3;
+String b1,b2,b3;
+String c1,c2,c3;
+String d1,d2,d3;
+String e1,e2,e3;
+String f1;
+String g1,g2,g3;
+
+Double n1 = 0.0, persen1;
+Double n2 = 0.0, persen2;
+Double n3 = 0.0, persen3;
+Double n4 = 0.0, persen4;
+Double n5 = 0.0, persen5;
+Double n6 = 0.0, persen6;
+Double n7 = 0.0, persen7;
+    
+}
