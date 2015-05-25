f0aa049caf5e56c4fab32c132232c1619c8fa1b3
Alfadin Arregres <alfadinsalim@gmail.com>
2015-05-25 20:56:40 +0700

    newest

---
+++ src/TugasBesar/KelasLauncher.java
@@ -0,0 +1,134 @@
+package TugasBesar;
+import java.io.*;
+
+public class KelasLauncher extends Kelas{
+	public static void main(String [] tugasbesar){
+		
+	/*identitasruang identitas = new identitasruang();
+	kondisiruang koruang = new kondisiruang();
+	kebersihan bersih = new kebersihan();
+	lingkungan lingkung = new lingkungan();
+	kenyamanan nyaman = new kenyamanan();
+	keamanan aman = new keamanan();
+	kebersihan bersih = new kebersihan();
+	///////////////////////////////////////////////////////////////
+	identitas.in();
+	koruang.in();
+	bersih.in();
+	lingkung.in();
+	nyaman.in();
+	aman.in();
+	
+	///////////////////////////////////////////////////////////////
+	identitas.proc();
+	koruang.proc();
+	bersih.proc();
+	lingkung.proc();
+	nyaman.proc();
+	aman.proc();
+	
+	///////////////////////////////////////////////////////////////
+	identitas.out();
+	koruang.out();
+	bersih.out();
+	lingkung.out();
+	nyaman.out();
+	aman.out();*/
+
+		//kenyamanan nyaman = new kenyamanan();
+		CCTV cctv = new CCTV();
+		Kelas kls = new Kelas();
+		//nyaman.in();
+		cctv.in();
+		//nyaman.proc();
+		cctv.proc();
+		//nyaman.out();
+		cctv.out();
+		try {
+			
+			FileWriter tulis = new FileWriter("save.txt");
+			BufferedWriter tulis2 = new BufferedWriter(tulis);
+			tulis2.write(""+cctv.result);
+			tulis2.newLine();
+			tulis2.write(""+cctv.result2);
+			tulis2.newLine();
+			tulis2.write(""+cctv.result3);
+			tulis2.newLine();
+			tulis2.write(""+cctv.result4);
+			tulis2.newLine();
+			tulis2.write(""+cctv.result5);
+			tulis2.newLine();
+			tulis2.close();
+		//ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("save.sf"));
+		//oos.writeObject(cctv);
+		//oos.writeObject(two);
+		//oos.writeObject(three);
+		//oos.close();
+		} catch(IOException ex) {
+		ex.printStackTrace();
+		}
+		//cctv = null;
+		//two = null;
+		//three = null;
+		
+		
+		System.out.println("\n\nDeserialized Object....\n");
+		try {
+			
+			File baca = new File("save.txt");
+			FileReader read = new FileReader(baca);
+			BufferedReader reader = new BufferedReader(read);
+			//reader.read();
+			//reader.readLine();
+			
+			
+			
+			//ObjectInputStream ois = new ObjectInputStream(new FileInputStream("save.sf"));
+			//CCTV cctvrestore = (CCTV) ois.readObject();
+		
+			String tampung = null;
+			while((tampung = reader.readLine()) != null){
+				System.out.println(tampung);
+			}
+		//System.out.println("One’s type: ");
+		reader.close();
+		
+		} catch(Exception ex) {
+		ex.printStackTrace();
+		}
+//=======
+            /*
+	kebersihan bersih = new kebersihan();
+	bersih.in();
+	bersih.proc();
+	bersih.out();
+        */
+            
+          /*  System.out.println("\n");
+            stopkontak steker = new stopkontak();
+            steker.in();
+            steker.proc();
+            steker.out();
+            
+            
+            System.out.println("\n");
+            LCD liquid = new LCD();
+            liquid.in();
+            liquid.proc();
+            liquid.out();
+            
+            
+            System.out.println("\n");
+            Lampu lmp = new Lampu();
+            lmp.in();
+            lmp.proc();
+            lmp.out();
+            
+            System.out.println("\n");
+            KipasAngin kps = new KipasAngin();
+            kps.in();
+            kps.proc();
+            kps.out();
+            */
+	}
+}
---
+++ src/TugasBesar/stopkontak.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasBesar;
+
+/**
+ *
+ * @author OWNER
+ */
+public class stopkontak extends Sarana {
+
+    @Override
+    void in() {
+        
+        System.out.print("\nJumlah Stop Kontak/Steker : ");
+	Double jumlahsteker = scan.nextDouble();
+	kls.setJumlahsteker(jumlahsteker);
+	
+	System.out.print("kondisi Steker : ");
+	String Kondisisteker = scan.next();
+	kls.setKondisisteker(Kondisisteker);
+	
+	System.out.print("posisi steker : ");
+	String Posisisteker = scan.next();
+	kls.setPosisisteker(Posisisteker);
+        
+    }
+
+    void proc() {
+        
+		if (kls.getJumlahsteker() >= 4) //1
+		{
+			
+			a1 = x;
+			n1 = n1 + 1;
+			if (kls.getKondisisteker().equalsIgnoreCase("Baik")) //2
+			{
+				a2 = x;
+				n1 = n1 + 1;	
+				if (kls.getPosisisteker().equalsIgnoreCase("Pojok")) //3
+				{
+					a3 = x;
+					n1 = n1 + 1;
+				}
+				else //3
+				{
+					a3 = y;
+					n1 = n1 + 0;
+					
+				}
+			}
+			else // 2 
+			{
+				a2 = y;
+				n1 = n1 + 0;
+				if (kls.getPosisisteker().equalsIgnoreCase("Pojok Ruang dan dekat dosen")) //3
+				{
+					a3 = x;
+					n1 = n1 + 1;
+					
+				}
+				else //3
+				{
+					a3 = y;
+					n1 = n1 + 0;
+					
+				}
+			}
+		}
+		else // 1 
+		{
+			
+			a1 = y;
+			n1 = n1 + 0;
+			if (kls.getKondisisteker().equalsIgnoreCase("Baik")) //2
+			{
+				a2 = x;
+				n1 = n1 + 1;	
+				if (kls.getPosisisteker().equalsIgnoreCase("Pojok Ruang dan dekat dosen")) //3
+				{
+					a3 = x;
+					n1 = n1 + 1;
+					
+				}
+				else //3
+				{
+					a3 = y;
+					n1 = n1 + 0;
+					
+				}
+			}
+			else // 2 
+			{
+				a2 = y;
+				n1 = n1 + 0;
+				if (kls.getPosisisteker().equalsIgnoreCase("Pojok Ruang dan dekat dosen")) //3
+				{
+					a3 = x;
+					n1 = n1 + 1;
+					
+				}
+				else //3
+				{
+					a3 = y;
+					n1 = n1 + 0;
+
+				}
+			}
+		}
+		persen1 = (n1*100)/3;
+        
+    }
+
+    @Override
+    void out() {
+        
+        System.out.print("\nJumlah Steker : "+kls.getJumlahsteker() +"  (" + ""+a1 +")");
+        
+        System.out.print("\nKondisi STeker : "+kls.getKondisisteker() +"  (" + ""+a3 +")");
+        
+        System.out.print("\nPosisi Steker : "+kls.getPosisisteker() +"  (" + ""+a2 +")");
+        
+        System.out.print("\nJumlah Sesuai = "+n1);
+	System.out.print("\nKesesuaian = "+persen1 + "%");
+    }
+    
+    
+}
